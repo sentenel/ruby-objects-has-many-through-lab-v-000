b58cf5e9701d28b5b9dae3b0bdcfa97d310ff65c
Seth Wax <sethlwax@gmail.com>
2018-04-08 23:46:11 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,15 @@
+class Doctor
+  attr_reader :name, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    self.appointments << appointment
+    appointment.doctor = self
+  end
+
+  
+end
